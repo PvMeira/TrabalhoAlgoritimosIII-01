8097ae9871cf3d3c774536b84c909f5d48c1b2cc
Pedro Vitor Meira Ciria <pv.ciria@gmail.com>
2017-04-24 18:41:59 -0300

    YYY

--- src/com/senac/br/core/main/Main.java
+++ src/com/senac/br/core/main/Main.java
@@ -9,9 +9,10 @@ public class Main {
     public static void main(String[] args) {
         String file = "/home/pvmeira/IdeaProjects/ProjetoAlgoritimosIII/resources/example_1.txt";
         MatrixUtils m = new MatrixUtils();
-        char[][] mmmmm = m.createMatrixFromTxt(file);
+
+        char[][] yyyyyy = m.createMatrixFromTxt(file);
         char[][] a22 = m.createMatrixFromTxt(file);
-        m.printMatrix("meh", mmmmm);
+        m.printMatrix("meh", yyyyyy);
         m.printMatrix("meh2", m.createNewMatrixFill0(a22));
 
     }
