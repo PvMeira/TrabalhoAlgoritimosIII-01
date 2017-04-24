4aa1d51b5294de5420b9000e99dbdc9342ac0997
Pedro Vitor Meira Ciria <pv.ciria@gmail.com>
2017-04-24 18:40:59 -0300

    commiting from teste branch2

--- src/com/senac/br/core/main/Main.java
+++ src/com/senac/br/core/main/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         String file = "/home/pvmeira/IdeaProjects/ProjetoAlgoritimosIII/resources/example_1.txt";
         MatrixUtils m = new MatrixUtils();
-        char[][] ggggg = m.createMatrixFromTxt(file);
+        char[][] mmmmm = m.createMatrixFromTxt(file);
         char[][] a22 = m.createMatrixFromTxt(file);
-        m.printMatrix("meh", ggggg);
+        m.printMatrix("meh", mmmmm);
         m.printMatrix("meh2", m.createNewMatrixFill0(a22));
 
     }
