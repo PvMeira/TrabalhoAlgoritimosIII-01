818b27100db34d26ed6dfda43ee3ac749323971e
Pedro Vitor Meira Ciria <pv.ciria@gmail.com>
2017-04-24 18:25:04 -0300

    sgsgs

--- src/com/senac/br/core/main/Main.java
+++ src/com/senac/br/core/main/Main.java
@@ -10,9 +10,9 @@ public class Main {
         String file = "/home/pvmeira/IdeaProjects/ProjetoAlgoritimosIII/resources/example_1.txt";
         MatrixUtils m = new MatrixUtils();
         char[][] meh = m.createMatrixFromTxt(file);
-        char[][] meh111 = m.createMatrixFromTxt(file);
+        char[][] meh11221 = m.createMatrixFromTxt(file);
         m.printMatrix("meh", meh);
-        m.printMatrix("meh2", m.createNewMatrixFill0(meh111));
+        m.printMatrix("meh2", m.createNewMatrixFill0(meh11221));
 
     }
 }
