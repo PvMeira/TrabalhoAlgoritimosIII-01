e54aec7bcb876087877feba4188db711434db1ff
Pedro Vitor Meira Ciria <pv.ciria@gmail.com>
2017-04-24 18:31:52 -0300

    Comit na teste

--- src/com/senac/br/core/main/Main.java
+++ src/com/senac/br/core/main/Main.java
@@ -9,9 +9,9 @@ public class Main {
     public static void main(String[] args) {
         String file = "/home/pvmeira/IdeaProjects/ProjetoAlgoritimosIII/resources/example_1.txt";
         MatrixUtils m = new MatrixUtils();
-        char[][] meh = m.createMatrixFromTxt(file);
+        char[][] 1331 = m.createMatrixFromTxt(file);
         char[][] a22 = m.createMatrixFromTxt(file);
-        m.printMatrix("meh", meh);
+        m.printMatrix("meh", 1331);
         m.printMatrix("meh2", m.createNewMatrixFill0(a22));
 
     }
